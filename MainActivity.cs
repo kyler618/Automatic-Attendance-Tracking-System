b4bd13921af53770e994ef7591d3ce62e592aa22
Kyler Tang <52806753+kyler618@users.noreply.github.com>
2022-06-28 17:14:39 +0800

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Acr.UserDialogs;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace AATS.Droid
+{
+    [Activity(Label = "AATS", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Forms.SetFlags("Shell_Experimental");
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            Forms.Init(this, savedInstanceState);
+            UserDialogs.Init(this);
+            LoadApplication(new App());
+        }
+    }
+}
